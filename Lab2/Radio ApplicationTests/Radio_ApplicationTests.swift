c44b4d538fd2b1faefa1c26d23a0fb90fcb88abd
Georgios Karakyklas <36900110+GeorgeKarakyklas@users.noreply.github.com>
2020-02-11 23:58:59 -0500

    Add files via upload

---
+++ Lab2/Radio ApplicationTests/Radio_ApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Radio_ApplicationTests.swift
+//  Radio ApplicationTests
+//
+//  Created by Karakyklas, Georgios on 1/31/20.
+//  Copyright © 2020 George Karakyklas. All rights reserved.
+//
+
+import XCTest
+@testable import Radio_Application
+
+class Radio_ApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
